102327b78812c6a111fedfb68c8dfc375dac591b
RavenVIP <ehsanjafary24@gmail.com>
2026-01-13 14:27:27 +0100

    update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>SvelteKit Test</h1>
+<p>Projekt von Ehsan. Deployment über GitHub und Vercel funktioniert.</p>
