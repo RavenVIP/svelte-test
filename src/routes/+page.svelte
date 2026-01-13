fc7ba87f0e105155c2f9876faa5f0c44a595d2f2
RavenVIP <ehsanjafary24@gmail.com>
2026-01-13 15:02:24 +0100

    add about page and layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+  let count = 0;
+</script>
+
 <h1>SvelteKit Test</h1>
 <p>Projekt von Ehsan. Deployment über GitHub und Vercel funktioniert.</p>
+
+<h1>SvelteKit Test</h1>
+<button on:click={() => count++}>
+  Klicks: {count}
+</button>
