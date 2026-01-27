673b2b3af4bf46a59e85780da549a593bb39834e
RavenVIP <ehsanjafary24@gmail.com>
2026-01-27 14:19:30 +0100

    add hello world

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,11 @@
 <h1>SvelteKit Test</h1>
 <p>Projekt von Ehsan. Deployment über GitHub und Vercel funktioniert.</p>
 
+<p>Hello World!</p>
+
 <h1>SvelteKit Test</h1>
 <button on:click={() => count++}>
   Klicks: {count}
 </button>
+
+//bau noch eine KI ein//
